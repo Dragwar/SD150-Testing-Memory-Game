3ba4db08a187489798aaa0d39ab0c201476fcbf3
Everett Grassler <dragwar9@gmail.com>
2019-01-11 10:33:44 -0600

    Added appSpec file and added todo list

---
+++ spec/appSpec.js
@@ -0,0 +1,11 @@
+/*
+  Exercise: Adding Jasmine to an existing project.
+  Add Jasmine to your existing 'Memory Game' project
+  Add a spec file that checks the following:
+
+  - Ensures the game starts with 16 cards
+  - Ensures that the cards all start closed
+  - Ensures that the move counter starts at 0
+  - Makes sure that after 2 cards are clicked, the move counter increases
+
+*/
